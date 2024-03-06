1538d9ed5f9e3f64e00f6e066ae809379daff537
Aurelien <aurelien.broncard@hotmail.fr>
2024-03-06 13:18:30 +0100

    Nouveau .gitignore et modification affichage_matrice.py
    Nouveau .gitignore et modification affichage_matrice.py (les valeurs des arcs sont les bonnes et affichage des colonnes juste

--- affichage_matrice.py
+++ affichage_matrice.py
@@ -1,6 +1,6 @@
 def afficher_matrice(graphe):
     # Déterminer le nombre de sommets dans le graphe
-    nb_sommets = len(graphe)
+    nb_sommets = len(graphe)-1
 
     # Initialiser la matrice avec des valeurs '*' pour représenter les arcs manquants
     matrice = [['*' for _ in range(nb_sommets + 1)] for _ in range(nb_sommets + 1)]
@@ -8,20 +8,20 @@ def afficher_matrice(graphe):
     # Remplir la matrice avec les valeurs des arcs
     for ligne in graphe:
         sommet = ligne[0]
-        valeurs_arcs = ligne[1]
         predecesseurs = ligne[2:]
 
         for pred in predecesseurs:
-            matrice[pred][sommet] = pred
+            valeurs_arcs = graphe[pred][1]
+            matrice[pred][sommet] = valeurs_arcs
 
     # Afficher la matrice des valeurs
     print("Matrice des valeurs")
-    print(" ", end="")
+    print("    ", end="")
     for i in range(nb_sommets + 1):
-        print(f"{i:3}", end="")
+        print(f"{str(i).center(3)}", end="")
     print()
-    for i in range(nb_sommets):
-        print(f"{i:2} ", end="")
+    for i in range(nb_sommets + 1):
+        print(f"{str(i).center(3)} ", end="")
         for j in range(nb_sommets + 1):
-            print(f"{matrice[i][j]:3}", end="")
+            print(f"{str(matrice[i][j]).center(3)}", end="")
         print()
